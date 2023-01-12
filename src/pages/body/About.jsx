6a37dd8fad5a5498a52868b4a566778b170737cb
cris-dangithub <cristiandaniel8080@gmail.com>
2023-01-12 18:16:56 -0500

    btn active

--- src/pages/body/About.jsx
+++ src/pages/body/About.jsx
@@ -47,18 +47,26 @@ const About = () => {
           possible results, and all of this entails being creative, responsible, proactive and, above all, having a good
           disposition for teamwork.
         </p>
-        <div className='about__btns-container'> {/* 200 ms */}
-          <button className='u-button about__btn' onClick={showTechnologies}>View technologies</button>
-          <button className='u-button about__btn' onClick={showStudies}>View studies</button>
+        <div className='about__btns-container'>
+          <button className={`u-button about__btn ${technologiesShowed ? 'u-button--active' : ''}`}
+            onClick={showTechnologies}
+          >
+            View technologies
+          </button>
+          <button className={`u-button about__btn ${!technologiesShowed ? 'u-button--active' : ''}`}
+            onClick={showStudies}
+          >
+            View studies
+          </button>
         </div>
       </main>
       <aside className={`about__right-content ${changingAbout ? 'u-inactive--right' : ''}`}>
-        <h3 className='about__sub-title'>{technologiesShowed ? 'Technologies': 'My studies'} </h3>
+        <h3 className='about__sub-title'>{technologiesShowed ? 'Technologies' : 'My studies'} </h3>
         <div className='about__cards-container'>
           {
             technologiesShowed ?
               technologies.map((technology, idx) => (
-                <CardTechnologies 
+                <CardTechnologies
                   key={idx}
                   info={technology}
                 />
