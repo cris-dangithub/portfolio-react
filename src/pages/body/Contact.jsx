89653d27d745fe458eece5d15e56842dd624bbe5
cris-dangithub <cristiandaniel8080@gmail.com>
2023-07-17 16:36:42 -0500

    emailJS added

--- src/pages/body/Contact.jsx
+++ src/pages/body/Contact.jsx
@@ -5,13 +5,27 @@ import Field from "../../components/forms/Field";
 import "./styles/Contact.css";
 import useDataBase from "../../hooks/useDataBase";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Contact = () => {
   const dispatch = useDispatch();
   const { contactMe } = useDataBase();
   const { register, handleSubmit } = useForm();
+
   const Submit = (data) => {
-    console.log(data);
+    const emailJSdata = {
+      service_id: "contact_service",
+      template_id: "contact_form",
+      user_id: "V8S5Jjaf39IHF_FHa",
+      template_params: {
+        ...data,
+        contact_number: (Math.random() * 100000).toFixed(0),
+      },
+    };
+    axios
+      .post("https://api.emailjs.com/api/v1.0/email/send", emailJSdata)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
 
   useEffect(() => {
@@ -21,7 +35,11 @@ const Contact = () => {
     <form className="contact" onSubmit={handleSubmit(Submit)}>
       <h2 className="about__title contact__title">Contact me</h2>
       {contactMe.map((fieldProps) => (
-        <Field key={fieldProps.title} infoProps={fieldProps} register={register} />
+        <Field
+          key={fieldProps.title}
+          infoProps={fieldProps}
+          register={register}
+        />
       ))}
       <button className="u-button">Enviar</button>
     </form>
